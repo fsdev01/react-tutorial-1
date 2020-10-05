cc2f37949ff00e1fbcf8a1c9d558c3889c7a2800
VH <vhuy371@gmail.com>
2020-10-05 19:36:01 +1100

    Updates

---
+++ precache-manifest.9680a6e875863a770ea1e46f4325b638.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec7dce562bba3c855117d99bccdabc16",
+    "url": "/react-tutorial-1/index.html"
+  },
+  {
+    "revision": "1fd3c1caaaeaabbf90b3",
+    "url": "/react-tutorial-1/static/css/main.f4d92d92.chunk.css"
+  },
+  {
+    "revision": "6d6ed16f790af6bea76b",
+    "url": "/react-tutorial-1/static/js/2.bf1aa842.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial-1/static/js/2.bf1aa842.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fd3c1caaaeaabbf90b3",
+    "url": "/react-tutorial-1/static/js/main.4f0f7c95.chunk.js"
+  },
+  {
+    "revision": "316927a377d276778758",
+    "url": "/react-tutorial-1/static/js/runtime-main.e5e754f5.js"
+  }
+]);
